7166c0a4a8cdcbf78129e378ccafa334cb41cbdc
leloufadel <leloufadel@gmail.com>
2024-02-17 13:29:55 +0000

    Update Generator

--- jmaatne/src/Generate.js
+++ jmaatne/src/Generate.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import quotes from './QuoteDB'
+import React from 'react';
+import quotes from './QuoteDB';
 
 export default function QuoteAndAuthor(props) {
-
     const { quote, generateRandomQuote } = props;
-    return (
-        <div className="card" >
 
-            <div className="card-body">
-                <p className="card-text">{quote.quote}</p>
-                <button
-                    onClick={() => { generateRandomQuote(quotes) }}
-                    type="submit">
-                    <i class="fas fa-mouse"></i> Generate Quote</button>
-                <button
-                    className="ml-3"
-                    onClick={() => {
-                        generateRandomQuote(quotes);
-                        window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(quote.quote + '--' + quote.author))
-                    }}
-                    type="submit"><i class="fab fa-twitter"></i> Share Quote</button>
+    return (
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                    <img src="img_avatar.png" alt="Avatar" style={{ width: "100px", height: "100px", marginTop: "10px" }} />
+                    <p style={{ width: "300px", height: "300px", marginTop: "60px" }}> Sewel had men eljema3a </p>
+                </div>
+                    <div className="flip-card-back">
+                        <p className="card-text">{quote.quote}</p>
+                        <button
+                            onClick={() => { generateRandomQuote(quotes) }}
+                            type="submit">
+                            <i className="fas fa-mouse"></i> Generate Quote
+                        </button>
+                        <button
+                            className="ml-3"
+                            onClick={() => {
+                                generateRandomQuote(quotes);
+                                window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(quote.quote))
+                            }}
+                            type="submit">
+                            <i className="fab fa-twitter"></i> Share Quote
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+    );
 }
